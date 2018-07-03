5a985fe593ae5cc7816f639e61c8954a94846b09
Ivan5 <converse_ivan@hotmail.com>
2018-07-03 18:42:16 -0500

    Add sequelize for models in server and valitaion with Join, in client add vutify for the style

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,12 +1,20 @@
 <template>
-  <div>
-    <h1>Register</h1>
-    <input type="email" name="email" v-model="email" placeholder="Enter email"><br>
+<v-layout colum>
+  <v-flex xs6 offset-xs3>
+    <div class="white elevation-2">
+      <v-toolbar flat dense class="red" darken-4>
+        <vtoolbar-title>Register</vtoolbar-title>
+      </v-toolbar>
+      <div class="pl-4 pr-4 pt-2 pb-2">
+        <input type="email" name="email" v-model="email" placeholder="Enter email"><br>
 
-    <input type="password" name="password" v-model="password" placeholder="Enter password"><br>
-    <button @click="register">Register</button>
-
-  </div>
+        <input type="password" name="password" v-model="password" placeholder="Enter password"><br>
+        <div class="error" v-html="error"></div><br>
+        <v-btn class="red" @click="register">Register</v-btn>
+      </div>
+    </div>
+  </v-flex>
+</v-layout>
 </template>
 
 <script>
@@ -15,23 +23,21 @@ export default {
   data () {
     return {
       email: '',
-      password: ''
+      password: '',
+      error:null
     }
   },
   methods:{
     async register () {
-      const response = await AuthenticationService.register({
-        email: this.email,
-        password: this.password
-      })
-      console.log(response.data);
-      
+      try {
+       await AuthenticationService.register({
+          email: this.email,
+          password: this.password
+       })
+      } catch (error) {
+        this.error = error.response.data.error
+      }
     }
-  },
-  mounted () {
-    setTimeout(() => {
-      this.email = 'hello world'
-    }, 200)
   }
 
 }
@@ -39,5 +45,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+.error{
+  color:red;
+}
 </style>
