1b2cc03bb8b34e153e2820b052f792af7e778f9c
Ivan5 <converse_ivan@hotmail.com>
2018-07-04 12:14:27 -0500

    Add jsonwebtoken to login functionality

--- server/src/controllers/AuthenticationController.js
+++ server/src/controllers/AuthenticationController.js
@@ -1,4 +1,13 @@
 const {User} = require('../models');
+const jwt = require('jsonwebtoken');
+const config = require('../config/config');
+
+function jwtSignUser(user){
+  const ONE_WEEK = 60 * 60 * 24 * 7;
+  return jwt.sign(user, config.authentication.jwtSecret, {
+    expiresIn: ONE_WEEK
+  })
+}
 
 module.exports = {
  async register(req,res){
@@ -9,7 +18,40 @@ module.exports = {
      res.status(400).send({
        error: 'this email account is already in use'
      })
-     //email already exists
+   
    }   
+  },
+
+  async login(req,res){
+    try {
+      const {email, password} = req.body
+      const user = await User.findOne({
+        where:{
+          email:email
+        }
+      })
+      if(!user){
+       return res.status(403).send({
+          error: 'The login information was incorrect'
+        })
+      }
+
+      const isPasswordValid = password === user.password
+      if(!isPasswordValid){
+        return res.status(403).send({
+          error: 'The login information was incorrect'
+        })
+      }
+      const userJson = user.toJSON()
+      res.send({
+        user: userJson,
+        token: jwtSignUser(userJson)
+      })
+    } catch (error) {
+      res.status(500).send({
+        error: 'An error has occured trying to log in'
+      })
+    
+    }
   }
 }
