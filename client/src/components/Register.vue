6686c3c6dc3c4de411e96f67f137129109fec58e
Ivan5 <converse_ivan@hotmail.com>
2018-07-03 23:43:28 -0500

    Add Register Component

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -2,15 +2,24 @@
 <v-layout colum>
   <v-flex xs6 offset-xs3>
     <div class="white elevation-2">
-      <v-toolbar flat dense class="red" darken-4>
+      <v-toolbar flat dense class="red darken-2" dark>
         <vtoolbar-title>Register</vtoolbar-title>
       </v-toolbar>
       <div class="pl-4 pr-4 pt-2 pb-2">
-        <input type="email" name="email" v-model="email" placeholder="Enter email"><br>
+        <v-text-field 
+          name="email"
+          type="email"
+          label="Email"
+          v-model="email"></v-text-field>
+        
+        <v-text-field 
+          name="password"
+          type="password"
+          label="Password"
+          v-model="password"></v-text-field>
 
-        <input type="password" name="password" v-model="password" placeholder="Enter password"><br>
         <div class="error" v-html="error"></div><br>
-        <v-btn class="red" @click="register">Register</v-btn>
+        <v-btn class="red darken-2" dark @click="register">Register</v-btn>
       </div>
     </div>
   </v-flex>
